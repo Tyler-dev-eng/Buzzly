338a058e0ea10c733f9b1f03d08f2c808f62b6b9
tyleravochoc <tyler@avochoc.com>
2026-02-25 14:20:52 +0200

    Some small changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,14 @@
+import 'package:buzzly/firebase_options.dart';
 import 'package:buzzly/providers/theme_provider.dart';
 import 'package:buzzly/services/auth/login_or_register_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown,
-  ]);
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const ProviderScope(child: MyApp()));
 }
